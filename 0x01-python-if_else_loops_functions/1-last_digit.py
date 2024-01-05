2b247c36d0f02e2975f6f6407b1c3d359dc625ac
lethurh <gugulethupraxedis@gmail.com>
2024-01-05 04:53:56 -0800

    Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number < 0:
 else:
     last digit = number % 10
     if last digit > 5:
-        print("last digit of {:d} is {:d} and is greater than 5". format(number, last digit), end="")
+        print("last digit of {:d} is {:d} and is greater than 5". format(number, last digit))
     elif last digit < 6 and last digit != 0:
         print("last digit of {:d} is {:d} and is less than 6 and not 0". format(number,last digit))
     else:
