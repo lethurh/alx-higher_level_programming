3a5ada59cae812f6bfb530a0f045b64dc13a9ec6
lethurh <gugulethupraxedis@gmail.com>
2024-01-05 04:59:36 -0800

    Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    lasti digit = number % -10
+    last digit = number % -10
 else:
     last digit = number % 10
     if last digit > 5:
