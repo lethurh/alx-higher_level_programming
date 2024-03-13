9f50e8526907207aa1e3cde40cd2ffa24cb67997
lethurh <gugulethupraxedis@gmail.com>
2024-03-13 06:04:50 -0700

    Write a script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Create database an user with read permisions
+-- Create database hbtn_0d_2 + a user to control it, both if nit already existing
 -- Execute: cat 2-create_read_user.sql | mysql -hlocalhost -uroot -p
 CREATE database IF NOT EXISTS hbtn_0d-2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@localhost IDENTFIED BY 'user_0d_2_pwd';
