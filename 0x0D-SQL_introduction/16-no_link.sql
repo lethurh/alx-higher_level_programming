60e2b62b97f3e0bd398566d93669c5de096be683
lethurh <gugulethupraxedis@gmail.com>
2024-03-13 04:58:52 -0700

    Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Lists all records of second_table where name is not NULL ordered by score
+-- Execute: cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT score, name FROM second_table WHERE name!= '' OR name IS NOT NULL ORDER BY score DESC;
