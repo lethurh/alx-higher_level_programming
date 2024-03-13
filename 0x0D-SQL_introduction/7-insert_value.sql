f9d4f26b3240d76f77d6bd0f48215dbd1bd13777
lethurh <gugulethupraxedis@gmail.com>
2024-03-13 01:20:58 -0700

    7. First add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Insert values
 -- Execute: cat 6-list_values.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Best School");
+INSERT INTO 'first_table' ('id', 'name') VALUES (89, 'Best School');
