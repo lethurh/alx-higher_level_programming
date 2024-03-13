aab2c99f45664158db6cb63c4ff7085fb6b6af91
lethurh <gugulethupraxedis@gmail.com>
2024-03-13 04:49:31 -0700

    Write a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
--- computes avarage score of all records in second_table
+-- computes avarage score of all records in second table
 -- Execute: cat 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 SELECT AVG(score) AS avarage FROM second_table;
