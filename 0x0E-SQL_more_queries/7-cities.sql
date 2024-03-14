93bbaebe30147a42152687ca2c5633efbe6bb4c6
lethurh <gugulethupraxedis@gmail.com>
2024-03-14 06:59:18 -0700

    7. Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+-- Creates at database cities and table inside database
+-- Execute: cat 7-cities.sql | mysql -hlocalhost -uroot -p
+CREATE database IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE table IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state id) REFERENCES states(id));
