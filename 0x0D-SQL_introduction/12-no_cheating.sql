e1dfa6f35e253c968cd92864072979c5d7a2e790
lethurh <gugulethupraxedis@gmail.com>
2024-03-15 11:56:28 -0700

    Write a script that updates the score of Bob to 10 in the table second_table.

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,2 @@
--- update data
--- Execute: cat 12-no_cheating.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-UPDATE 'second_table' SET 'score' = 10 WHERE 'second_table'.'name' = "Bob";
+-- updates the score of Bob to 10 in the table second_table
+UPDATE second_table SET score = 10 WHERE name = "Bob";
