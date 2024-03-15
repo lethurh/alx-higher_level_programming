d4f73adcd4ee4312252d30cdfe536559c8d3f2b9
lethurh <gugulethupraxedis@gmail.com>
2024-03-15 11:44:18 -0700

    7. First add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,6 @@
--- Insert values
--- Execute: cat 6-list_values.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, 'Best School');
+-- a script that inserts a new row in the table first_table
+-- (database hbtn_0c_0) in your MySQL server.
+INSERT INTO first_table
+    (id, name)
+VALUES
+    (89, 'Best School');
